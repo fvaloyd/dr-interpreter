f46647669eb65c876f82a629e3288b1d6c0bda8f
Francis Valoy <francisalcantara001@gmail.com>
2024-05-05 20:16:55 -0400

    convert RLPL to RPPL

--- src/Repl.cs
+++ src/Repl.cs
@@ -2,19 +2,51 @@ namespace Interpreter;
 
 public record Repl
 {
+
+    public const string SHARK_ASCII = """"""
+                                         ,-
+                                       ,'::|
+                                      /::::|
+                                    ,'::::o\                                      _..
+                 ____........-------,..::?88b                                  ,-' /
+         _.--"""". . . .      .   .  .  .  ""`-._                           ,-' .;'
+        <. - :::::o......  ...   . . .. . .  .  .""--._                  ,-'. .;'
+         `-._  ` `":`:`:`::||||:::::::::::::::::.:. .  ""--._ ,'|     ,-'.  .;'
+             """_=--       //'doo.. ````:`:`::::::::::.:.:.:. .`-`._-'.   .;'
+                 ""--.__     P(       \               ` ``:`:``:::: .   .;'
+                        "\""--.:-.     `.                             .:/
+                          \. /    `-._   `.""-----.,-..::(--"".\""`.  `:\
+                           `P         `-._ \          `-:\          `. `:\
+                                           ""            "            `-._)
+        """""";
+
     public static void Start()
     {
         Console.WriteLine("Type in commands");
-        Console.Write(">> ");
-        string line = Console.ReadLine() ?? throw new Exception();
-
-        var l = Lexer.Create(line);
-
-        var tok = l.NextToken();
-        while (tok.Type.Value != Token.EOF)
+        while (true)
         {
-            Console.WriteLine(tok);
-            tok = l.NextToken();
+            Console.Write(">> ");
+            string line = Console.ReadLine() ?? throw new Exception();
+
+            Lexer l = Lexer.Create(line);
+            Parser p = new Parser(l);
+            Program pr = p.ParseProgram();
+
+            if (p.Errors.Any())
+            {
+                PrintParserErros(p.Errors);
+                continue;
+            }
+
+            Console.WriteLine(pr.String());
         }
+    }
+
+    static void PrintParserErros(List<string> errors)
+    {
+        Console.WriteLine(SHARK_ASCII);
+        Console.WriteLine("Woops! We ran into some shark business here!");
+        Console.WriteLine(" parser errors:");
+        errors.ForEach(Console.WriteLine);
     }
 }
