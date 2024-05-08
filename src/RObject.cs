f63c82e758a3df8a5035f5824926dfbb06a8f76d
Francis Valoy <francisalcantara001@gmail.com>
2024-05-08 00:19:03 -0400

    add Integer, Boolean and null ROs

--- src/RObject.cs
+++ src/RObject.cs
@@ -2,8 +2,39 @@ namespace Interpreter;
 
 using ObjectType = string;
 
-public interface RObject
+public abstract record _Object
 {
-    ObjectType Type();
-    string Inspect();
+    public const string INTEGER_OBJ = "INTEGER";
+    public const string BOOLEAN_OBJ = "BOOLEAN";
+    public const string NULL_OBJ = "NULL";
+
+    public abstract ObjectType Type();
+    public abstract string Inspect();
+}
+
+public record Integer(Int64 Value) : _Object
+{
+    public override string Inspect()
+        => Value.ToString();
+
+    public override ObjectType Type()
+        => INTEGER_OBJ;
+}
+
+public record _Boolean(bool Value) : _Object
+{
+    public override string Inspect()
+        => Value.ToString().ToLower();
+
+    public override ObjectType Type()
+        => BOOLEAN_OBJ;
+}
+
+public record _Null : _Object
+{
+    public override string Inspect()
+        => "null";
+
+    public override ObjectType Type()
+        => NULL_OBJ;
 }
